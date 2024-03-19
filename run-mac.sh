31100738d56e4a1ebf1b7d06348ef02e332b1b41
lavrentious <lavrent0@yandex.ru>
2024-03-19 10:03:42 +0300

    fix: port

--- run-mac.sh
+++ run-mac.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 dir=$(pwd)
-port=5666
+port=4564
 port=$port ./gradlew build && (osascript -e "tell application \"Terminal\" to do script \"cd '$dir' && port=$port java -jar apps/client/build/libs/lab6Client.jar\"" ; port=$port java -jar apps/server/build/libs/lab6Server.jar)
